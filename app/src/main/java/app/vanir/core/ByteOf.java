b12b5f095d44cbae7062e696b3a51b2240a5625d
Chris Pro <prokic720x@gmail.com>
2019-02-04 16:15:17 +0100

    Add files via upload

---
+++ app/src/main/java/app/vanir/core/ByteOf.java
@@ -0,0 +1,18 @@
+package app.vanir.core;
+
+public final class ByteOf {
+
+        public static final int CHAR = 2;
+        public static final int DOUBLE = 8;
+        public static final int FLOAT = 4;
+        public static final int INT = 4;
+        public static final int LONG = 8;
+        public static final int SHORT = 2;
+        public static final int DIV = 6;
+        public static final int NOR = 2;
+
+
+        private ByteOf() {
+        }
+
+}
---
+++ app/src/main/java/app/vanir/package_/PackageV.java
@@ -0,0 +1,32 @@
+package app.vanir.package_;
+
+import com.google.android.gms.stats.CodePackage;
+import com.google.android.gms.*;
+import java.lang.annotation.Annotation;
+import java.net.InetAddress;
+
+/**
+ * Vanir Package information
+ */
+public class PackageV {
+  public  final String COMMON;
+  public  final String GCM;
+  public  final String LOCATION;
+  public  final String LOCATION_SHARING;
+  public  final String OTA;
+  public  final String SECURITY;
+
+  public PackageV(String COMMON, String GCM, String LOCATION, String LOCATION_SHARING,String OTA, String SECURITY ) {
+    this.COMMON = "COMMON";
+    this.GCM = "GCM";
+    this.LOCATION = "LOCATION";
+    this.LOCATION_SHARING = "LOCATION_SHARING";
+    this.OTA = "OTA";
+    this.SECURITY = "SECURITY";
+  }
+
+  public String toString() {
+    return String.format("PacInfo: { LOCATION_SHARING=%d, GCM=%d, LOCATION=%s, COMMON='%s', SECURITY='%s' }", LOCATION_SHARING, GCM, LOCATION, COMMON, SECURITY);
+  }
+
+}
