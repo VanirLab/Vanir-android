2e0a451221f01cd668ea993191a140f9700874bb
Chris Pro <prokic720x@gmail.com>
2019-11-24 15:08:49 +0100

    Create PackageVerificationEncryption.java

---
+++ app/src/main/java/app/vanir/package_/PackageVerificationEncryption.java
@@ -0,0 +1,11 @@
+package app.vanir.package_.;
+
+
+public void PackageVerificationEncryption(Uri packageURI,
+ IPackageInstallObserver observer, int flags,
+ String installerPackageName,
+ VerificationParams verificationParams,
+ 
+ ContainerEncryptionParams encryptionParams) {
+
+}
